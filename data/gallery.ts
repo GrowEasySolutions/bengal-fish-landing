e5322bf5b3a6508038595d54e64c61c79b6c9d07
GrowEasySolutions <sab.meherzad@gmail.com>
2025-09-22 06:25:44 -0700

    Update gallery.ts

--- data/gallery.ts
+++ data/gallery.ts
@@ -1,17 +1,18 @@
-// /data/gallery.ts
 export type GalleryImage = { src: string; alt: string };
 
 export const galleryImages: GalleryImage[] = [
-  { src: "/images/gallery/gallery-01-biryani-topdown.jpg", alt: "Top-down chicken biryani" },
-  { src: "/images/gallery/gallery-02-kathi-roll-cut.jpg", alt: "Kathi roll cross-section" },
-  { src: "/images/gallery/gallery-03-mixed-grill.jpg", alt: "Mixed grill platter" },
-  { src: "/images/gallery/gallery-04-butter-chicken.jpg", alt: "Butter chicken with cream swirl" },
-  { src: "/images/gallery/gallery-05-spice-bowls.jpg", alt: "Spice bowls flatlay" },
-  { src: "/images/gallery/gallery-06-naan-basket.jpg", alt: "Naan basket with ghee" },
-  { src: "/images/gallery/gallery-07-chutneys.jpg", alt: "Trio of chutneys in bowls" },
-  { src: "/images/gallery/gallery-08-samosa.jpg", alt: "Samosas with chutney" },
-  { src: "/images/gallery/gallery-09-tea.jpg", alt: "Steaming chai in a glass cup" },
-  { src: "/images/gallery/gallery-10-dessert.jpg", alt: "Bengali dessert with pistachio garnish" },
-  { src: "/images/gallery/gallery-11-family-table.jpg", alt: "Shared table of Bengali dishes" },
-  { src: "/images/gallery/gallery-12-ingredients.jpg", alt: "Fresh herbs, chilies and lemon" },
+  { src: "/images/gallery/mahi-mahi-fry.jpg", alt: "Mahi Mahi Fish Fry" },
+  { src: "/images/gallery/goat-curry.jpg", alt: "Goat Curry Bhuna" },
+  { src: "/images/gallery/chicken-curry.jpg", alt: "Chicken Curry" },
+  { src: "/images/gallery/veg-biryani.jpg", alt: "Vegetable Biryani" },
+  { src: "/images/gallery/chicken-pakora.jpg", alt: "Chicken Pakora" },
+  { src: "/images/gallery/bengal-fish-pakora.jpg", alt: "Bengal Fish Pakora" },
+  { src: "/images/gallery/paneer-tikka.jpg", alt: "Paneer Tikka" },
+  { src: "/images/gallery/chilli-fish-manchurian.jpg", alt: "Chilli Fish Manchurian" },
+
+  // Acceptable non-dish shots that do not misrepresent offerings:
+  { src: "/images/gallery/spice-bowls.jpg", alt: "Spice bowls flatlay" },
+  { src: "/images/gallery/naan-basket.jpg", alt: "Naan basket" },
+  { src: "/images/gallery/chutneys.jpg", alt: "Chutneys trio" },
+  { src: "/images/gallery/tea.jpg", alt: "Chai tea in a glass" },
 ];
